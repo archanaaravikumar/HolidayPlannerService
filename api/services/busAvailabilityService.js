2c4c5f7d4b9ab8a86bdbc53593c15dc173a0fe8c
PoojaS <poojas2406@gmail.com>
2018-04-13 10:53:13 +0530

    Pooja | Integrate with goibio apis to fetch bus details from a given start and end point

--- api/services/busAvailabilityService.js
+++ api/services/busAvailabilityService.js
@@ -2,19 +2,20 @@ var functions = function (gatewayService) {
 
     function fetch(from, to, journeyDate) {
         console.log("Fetching buses");
-        let result = gatewayService.getBuses(from, to, journeyDate);
-        let buses = [];
-        const onwardBuses = result.data.onwardflights;
+        return gatewayService.getBuses(from, to, journeyDate).then(result => {
+            let buses = [];
+            const onwardBuses = result.data.onwardflights;
 
-        if (!onwardBuses || !onwardBuses.length)
+            if (!onwardBuses || !onwardBuses.length)
+                return buses;
+
+            onwardBuses.slice(0, 5).forEach(element => {
+                buses.push(`${element.TravelsName} ${element.BusType} starts at ${element.DepartureTime} for ${element.fare.totalfare} Rupees`);
+            });
+
             return buses;
-
-        onwardBuses.slice(0, 5).forEach(element => {
-            buses.push(`${element.TravelsName} ${element.BusType} starts at ${element.DepartureTime} for ${element.fare.totalfare} Rupees`);
         });
-
-        return buses;
-    };
+    }
 
     return {
         fetch: fetch
